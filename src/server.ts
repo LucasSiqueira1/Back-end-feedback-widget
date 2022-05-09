5fb80ca01652d7e39da3800b910243843910994c
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-09 14:17:40 -0300

    banco de dados prisma

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json()); // para o express entender o JSON, antes da rota!
 
-app.post('/feedbacks', (req, res) => {
-    console.log(req.body);
-    return res.send('Hello World');
+app.post('/feedbacks', async (req, res) => {
+    // console.log(req.body);
+    const feedback = await prisma.feedback.create({
+        data: {
+            type: req.body.type,
+            comment: req.body.comment,
+            screenshot: req.body.screenshot,
+        }
+    })
+
+    return res.status(201).json({data: feedback});
 })
 
 app.listen(3333, () => {
