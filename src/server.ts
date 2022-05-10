8939c8f1813c3c38051f7337b17b0e8e32b2c056
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-10 08:49:48 -0300

    banco de dados prisma

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json()); // para o express entender o JSON, antes da rota!
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "7aa6ce92cb04ca",
+        pass: "ee687bb42bb22d"
+    }
+});
 
 app.post('/feedbacks', async (req, res) => {
     // console.log(req.body);
@@ -15,7 +25,19 @@ app.post('/feedbacks', async (req, res) => {
         }
     })
 
-    return res.status(201).json({data: feedback});
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Lucas Eduardo <lucaraz133@gmail.com>',
+        subject: 'Feedback do usuário',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback ${req.body.type}</p>`,
+            `<p>Comentário do feedback ${req.body.comment}</p>`,
+            `</div>`
+        ].join('\n')
+    })
+
+    return res.status(201).json({ data: feedback });
 })
 
 app.listen(3333, () => {
