5ac11c16d4df40ef28d9b33a79972f37f09156f2
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-10 11:40:15 -0300

    jest, testes em cada propriedade do feedback

--- src/functions/submit-feedback.test.ts
+++ src/functions/submit-feedback.test.ts
@@ -5,6 +5,7 @@
 import { SubmitFeedback } from "./submit-feedback"
 
 describe('Submit feedback' , () => {
+  //teste geral do feedback
   it('should be able to submit a feedback', async() => {
     const submitFeedback = new SubmitFeedback({create: async() => {}}, {sendMail: async() => {}})
 
@@ -13,5 +14,44 @@ describe('Submit feedback' , () => {
       comment: 'This is a bug',
       screenshot: 'data:image/png;base64,81293adjsahdjaisd'
     })).resolves.not.toThrow();
+  })
+  ///////////////////////////////////////////////////////////////////////////////////////////////
+
+  
+  //teste do tipo de feedback
+  it('should not be able to submit feedback without type', async() => {
+    const submitFeedback = new SubmitFeedback({create: async() => {}}, {sendMail: async() => {}})
+
+    await expect(submitFeedback.execute({
+      type: '',
+      comment: 'This is a bug',
+      screenshot: 'data:image/png;base64,81293adjsahdjaisd'
+    })).rejects.toThrow();
+  })
+  ///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  //teste do comentário do feedback
+  it('should not be able to submit feedback without comment', async() => {
+    const submitFeedback = new SubmitFeedback({create: async() => {}}, {sendMail: async() => {}})
+
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: '',
+      screenshot: 'data:image/png;base64,81293adjsahdjaisd'
+    })).rejects.toThrow();
+  })
+  ///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  //teste da screenshot do feedback
+  it('should not be able to submit feedback with an invalid screenshot', async() => {
+    const submitFeedback = new SubmitFeedback({create: async() => {}}, {sendMail: async() => {}})
+
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'This is a bug',
+      screenshot: 'teste.jpg'
+    })).rejects.toThrow();
   })
 })
