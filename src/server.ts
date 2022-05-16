bc9aab081a66da4a4697f6609a3a6fb92ec29de1
LucasSiqueira1 <lucas_siqueira28@outlook.com>
2022-05-16 09:43:55 -0300

    configurando variavel da porta

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json()); // para o express entender o JSON, antes da rota!
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('Server started on port 3333');
 });
